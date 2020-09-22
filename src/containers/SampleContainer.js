1dadc8014a7604bd6aac6086b52349e7b9e389a7
ldg <7sus7@naver.com>
2020-09-22 15:46:45 +0900

    redux-thunk를 이용한 API 비동기 요청 완료

---
+++ src/containers/SampleContainer.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { connect } from "react-redux";
+import Sample from "../components/Sample";
+import { getPost, getUsers } from "../modules/sample";
+
+const { useEffect } = React;
+const SampleContainer = ({
+  getPost,
+  getUsers,
+  post,
+  users,
+  loadingPost,
+  loadingUSers,
+}) => {
+  useEffect(() => {
+    getPost(1);
+    getUsers(1);
+  }, [getPost, getUsers]);
+  return (
+    <Sample
+      post={post}
+      users={users}
+      lodadingPost={loadingPost}
+      loaddingUsesr={loadingUSers}
+    />
+  );
+};
+
+export default connect(
+  ({ sample }) => ({
+    post: sample.post,
+    users: sample.users,
+    loadingPost: sample.loading.GET_POST,
+    loadingUSers: sample.loading.GET_USERSs,
+  }),
+  {
+    getPost,
+    getUsers,
+  }
+)(SampleContainer);
